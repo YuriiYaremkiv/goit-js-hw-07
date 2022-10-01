06b9d779037ecd6d125e0cc1318c2bee93fc55b9
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-10-02 00:52:49 +0300

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+}
+
+refs.gallery.addEventListener('click', getUrlImage);
+
+function getUrlImage(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`)
+
+instance.show()
+
+};
+
+
+
+const cardsImages = createCardImageMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', cardsImages);
+
+
+function createCardImageMarkup (images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>   
+        `}).join('');
+};
+
