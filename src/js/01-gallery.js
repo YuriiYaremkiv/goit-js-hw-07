3509f1a18133e4c7340b1e88624d367817c21e40
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-10-02 15:37:54 +0300

    create task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,40 +5,51 @@ const refs = {
     gallery: document.querySelector('.gallery'),
 }
 
-refs.gallery.addEventListener('click', getUrlImage);
+let instance ='';
 
-function getUrlImage(event) {
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    event.preventDefault();
-
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">`)
-
-instance.show()
-
-};
+// create images list
+const cardsImage = createCardImageMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', cardsImage);
 
 
+refs.gallery.addEventListener('click', showImage);
 
-const cardsImages = createCardImageMarkup(galleryItems);
-refs.gallery.insertAdjacentHTML('beforeend', cardsImages);
+document.addEventListener("keydown", closeImageModalWindow);
 
 
 function createCardImageMarkup (images) {
     return images.map(({preview, original, description}) => {
         return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-                />
-            </a>
-        </div>   
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>   
         `}).join('');
 };
 
+function showImage(e) {
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    e.preventDefault();
+
+    openModalWindow(e);
+};
+
+function openModalWindow (urlImage) {
+    return instance = basicLightbox.create(`<img src="${urlImage.target.dataset.source}" width="800" height="600">`), instance.show();
+}
+
+function closeImageModalWindow(e) {
+    if (e.key !== 'Escape') {
+        return;
+    }
+
+    instance.close();
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
 
-console.log(galleryItems);
+
+refs.gallery.insertAdjacentHTML('beforeend', createCardImageMarkup(galleryItems));
+
+function createCardImageMarkup (images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+                 src="${preview}" 
+                 alt="${description}"/>
+        </a>  
+        `}).join('');
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
